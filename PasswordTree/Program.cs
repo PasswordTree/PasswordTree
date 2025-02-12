f3917a2954c2d9219a363a65ce569423f2c2617e
ALiMoradzade <alimooradzade@gmail.com>
2025-02-12 22:24:49 +0330

    Set Password Generator

--- PasswordTree/Program.cs
+++ PasswordTree/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PasswordTree.Password_Generator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace PasswordTree
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PasswordGeneratorForm());
         }
     }
 }
