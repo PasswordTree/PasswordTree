5a2f5d92b5f09cba21f90563006a49dfeb3f5011
ALiMoradzade <alimooradzade@gmail.com>
2025-02-12 22:30:19 +0330

    UI Changes

--- PasswordTree/Password Generator/PasswordGeneratorForm.Designer.cs
+++ PasswordTree/Password Generator/PasswordGeneratorForm.Designer.cs
@@ -28,12 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxCurrentPassword = new System.Windows.Forms.TextBox();
+            this.buttonGeneratePassword = new System.Windows.Forms.Button();
+            this.textBoxPreviousPasswords = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingToolStripMenuItem
+            // 
+            this.settingToolStripMenuItem.Name = "settingToolStripMenuItem";
+            this.settingToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.settingToolStripMenuItem.Text = "Setting";
+            // 
+            // textBoxCurrentPassword
+            // 
+            this.textBoxCurrentPassword.Location = new System.Drawing.Point(12, 27);
+            this.textBoxCurrentPassword.Multiline = true;
+            this.textBoxCurrentPassword.Name = "textBoxCurrentPassword";
+            this.textBoxCurrentPassword.Size = new System.Drawing.Size(443, 192);
+            this.textBoxCurrentPassword.TabIndex = 2;
+            // 
+            // buttonGeneratePassword
+            // 
+            this.buttonGeneratePassword.Location = new System.Drawing.Point(380, 225);
+            this.buttonGeneratePassword.Name = "buttonGeneratePassword";
+            this.buttonGeneratePassword.Size = new System.Drawing.Size(75, 23);
+            this.buttonGeneratePassword.TabIndex = 3;
+            this.buttonGeneratePassword.Text = "Generate";
+            this.buttonGeneratePassword.UseVisualStyleBackColor = true;
+            // 
+            // textBoxPreviousPasswords
+            // 
+            this.textBoxPreviousPasswords.Location = new System.Drawing.Point(486, 27);
+            this.textBoxPreviousPasswords.Multiline = true;
+            this.textBoxPreviousPasswords.Name = "textBoxPreviousPasswords";
+            this.textBoxPreviousPasswords.Size = new System.Drawing.Size(302, 173);
+            this.textBoxPreviousPasswords.TabIndex = 4;
+            // 
+            // PasswordGeneratorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 269);
+            this.Controls.Add(this.textBoxPreviousPasswords);
+            this.Controls.Add(this.buttonGeneratePassword);
+            this.Controls.Add(this.textBoxCurrentPassword);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "PasswordGeneratorForm";
             this.Text = "PasswordGeneratorForm";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxCurrentPassword;
+        private System.Windows.Forms.Button buttonGeneratePassword;
+        private System.Windows.Forms.TextBox textBoxPreviousPasswords;
     }
 }
