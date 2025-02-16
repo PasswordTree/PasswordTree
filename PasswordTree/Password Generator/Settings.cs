8ee31cfee236954c1f3e966596a43d419b4ba8af
ALiMoradzade <alimooradzade@gmail.com>
2025-02-16 17:46:36 +0330

    added Settings

---
+++ PasswordTree/Password Generator/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PasswordTree.Password_Generator
+{
+
+    internal static class Settings
+    {
+        internal static class PasswordCatagory
+        {
+            public static int Maximum { get; set; }
+            public static int Count { get; set; }
+        }
+
+        internal static class Password
+        {
+            public static int Maximum { get; set; }
+            public static int Count { get; set; }
+            public static bool IsDistinct { get; set; }
+        }
+    }
+}
